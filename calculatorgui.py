1106a16ac47fd8ec33e6c0cca880a1834a7ef313
Shreejan <dolaishreejan@gmail.com>
2021-01-29 12:08:11 +0530

    Initial commit

--- calculatorgui.py
+++ calculatorgui.py
@@ -1,148 +1,107 @@
 from tkinter import *
 
-root = Tk()
-#button_9 = Button(label_key,text='9',height=3,width=5,font=('Helvetica','12'))
-#button_9.grid(row=0,column=0)
-class Calculator:
-    def click_button(self,numbers):
-        global operator
-        global var
-        self.operator = self.operator + str(numbers)
-        self.var.set(self.operator)
-
-    def clear(self):
-        self.entry.delete(0,END)
-        self.operator =""
-
-    ''' def delete(self):
-        self.operator = str(self.entry.delete(len(self.entry.get())-1))
-    '''
+win = Tk() # This is to create a basic window
+win.geometry("312x324")  # this is for the size of the window 
+win.resizable(0, 0)  # this is to prevent from resizing the window
+win.title("Calculator")
 
 
-    def evaluate(self):
-        self.answer =eval(self.entry.get())
-        self.var.set(self.answer)
-        self.operator = str(self.answer)
+ 
 
-    def __init__(self,master):
-
-        self.operator = ""
-        self.var = StringVar()
-        frame_s = Frame(master, height=400, width=45 )
-        frame_s.pack(side=TOP, fill=BOTH, expand=True)
-        self.entry = Entry(frame_s,textvariable=self.var,bg='grey',width=45,bd=20,insertwidth=4,justify='right',font=('arial',10,'bold'))
-        self.entry.pack()
-        self.t = Text(self.entry,height=40)
+def btn_click(item):
+    global expression
+    expression = expression + str(item)
+    input_text.set(expression)
+    # This Function continuously updates the 
+    # input field whenever you enters a number
 
 
 
-        label_key = Label(root, height=15, width=30,bd=10,bg='gray50')
-        label_key.pack(side=LEFT, fill=BOTH, expand=True)
+def bt_clear(): 
+    global expression 
+    expression = "" 
+    input_text.set("")
+    # 'bt_clear' function :This is used to clear 
+    # the input field
 
-        label_fkey = Label(root, height=15, width=15, bg='gray25')
-        label_fkey.pack(fill=BOTH, expand=True)
+ 
+# 'bt_equal':This method calculates the expression 
+# present in input field
+ 
+def bt_equal():
+    global expression
+    result = str(eval(expression)) # 'eval':This function is used to evaluates the string expression directly
+    input_text.set(result)
+    expression = ""
+ 
+expression = ""
+ 
+# 'StringVar()' :It is used to get the instance of input field
+ 
+input_text = StringVar()
+ 
+# Let us creating a frame for the input field
+ 
+input_frame = Frame(win, width=312, height=50, bd=0, highlightbackground="black", highlightcolor="black", highlightthickness=2)
+ 
+input_frame.pack(side=TOP)
+ 
+#Let us create a input field inside the 'Frame'
+ 
+input_field = Entry(input_frame, font=('arial', 18, 'bold'), textvariable=input_text, width=50, bg="#eee", bd=0, justify=RIGHT)
+ 
+input_field.grid(row=0, column=0)
+ 
+input_field.pack(ipady=10) 
+ 
+#Let us creating another 'Frame' for the button below the 'input_frame'
+ 
+btns_frame = Frame(win, width=312, height=272.5, bg="grey")
+ 
+btns_frame.pack()
+ 
+# first row
+ 
+clear = Button(btns_frame, text = "C", fg = "black", width = 32, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: bt_clear()).grid(row = 0, column = 0, columnspan = 3, padx = 1, pady = 1)
+ 
+divide = Button(btns_frame, text = "/", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("/")).grid(row = 0, column = 3, padx = 1, pady = 1)
+ 
+# second row
+ 
+seven = Button(btns_frame, text = "7", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(7)).grid(row = 1, column = 0, padx = 1, pady = 1)
+ 
+eight = Button(btns_frame, text = "8", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(8)).grid(row = 1, column = 1, padx = 1, pady = 1)
+ 
+nine = Button(btns_frame, text = "9", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(9)).grid(row = 1, column = 2, padx = 1, pady = 1)
+ 
+multiply = Button(btns_frame, text = "*", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("*")).grid(row = 1, column = 3, padx = 1, pady = 1)
+ 
+# third row
+ 
+four = Button(btns_frame, text = "4", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(4)).grid(row = 2, column = 0, padx = 1, pady = 1)
+ 
+five = Button(btns_frame, text = "5", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(5)).grid(row = 2, column = 1, padx = 1, pady = 1)
+ 
+six = Button(btns_frame, text = "6", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(6)).grid(row = 2, column = 2, padx = 1, pady = 1)
+ 
+minus = Button(btns_frame, text = "-", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("-")).grid(row = 2, column = 3, padx = 1, pady = 1)
+ 
+# fourth row
+ 
+one = Button(btns_frame, text = "1", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(1)).grid(row = 3, column = 0, padx = 1, pady = 1)
+ 
+two = Button(btns_frame, text = "2", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(2)).grid(row = 3, column = 1, padx = 1, pady = 1)
+ 
+three = Button(btns_frame, text = "3", fg = "black", width = 10, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(3)).grid(row = 3, column = 2, padx = 1, pady = 1)
+ 
+plus = Button(btns_frame, text = "+", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click("+")).grid(row = 3, column = 3, padx = 1, pady = 1)
+ 
 
-        label_7 = Label(label_key, bg='black')
-        label_7.grid(row=0, column=0)
-        button_7 = Button(label_7, text='7', font=('Helvetica', '16'),command= lambda : self.click_button(7),bg='black',fg='cyan')
-        button_7.pack()
-
-        label_8 = Label(label_key, bg='black')
-        label_8.grid(row=0, column=1, padx=20)
-        button_8 = Button(label_8, text='8', font=('Helvetica', '16'),command= lambda: self.click_button(8),bg='black',fg='cyan')
-        button_8.pack()
-
-        label_9 = Label(label_key, bg='black')
-        label_9.grid(row=0, column=2, padx=10)
-        button_9 = Button(label_9, text='9', font=('Helvetica', '16'),command= lambda: self.click_button(9),bg='black',fg='cyan')
-        button_9.pack()
-
-        label_4 = Label(label_key, bg='black')
-        label_4.grid(row=1, column=0, padx=10, pady=10)
-        button_4 = Button(label_4, text='4', font=('Helvetica', '16'),command= lambda: self.click_button(4),bg='black',fg='cyan')
-        button_4.pack()
-
-        label_5 = Label(label_key, bg='black')
-        label_5.grid(row=1, column=1, padx=10, pady=10)
-        button_5 = Button(label_5, text='5', font=('Helvetica', '16'),command= lambda: self.click_button(5),bg='black',fg='cyan')
-        button_5.pack()
-
-        label_6 = Label(label_key, bg='black')
-        label_6.grid(row=1, column=2, padx=10, pady=10)
-        button_6 = Button(label_6, text='6', font=('Helvetica', '16'),command= lambda: self.click_button(6),bg='black',fg='cyan')
-        button_6.pack()
-
-        label_1 = Label(label_key, bg='black')
-        label_1.grid(row=2, column=0, padx=10)
-        button_1 = Button(label_1, text='1', font=('Helvetica', '16'),command= lambda: self.click_button(1),bg='black',fg='cyan')
-        button_1.pack()
-
-        label_2 = Label(label_key, bg='black')
-        label_2.grid(row=2, column=1, padx=10)
-        button_2 = Button(label_2, text='2', font=('Helvetica', '16'),command= lambda: self.click_button(2),bg='black',fg='cyan')
-        button_2.pack()
-
-        label_3 = Label(label_key, bg='black')
-        label_3.grid(row=2, column=2, padx=10)
-        button_3 = Button(label_3, text='3', font=('Helvetica', '16'),command= lambda: self.click_button(3),bg='black',fg='cyan')
-        button_3.pack()
-
-        label_0 = Label(label_key, bg='black')
-        label_0.grid(row=3, column=0, padx=10, pady=10)
-        button_0 = Button(label_0, text='0', font=('Helvetica', '16'),command= lambda: self.click_button(0),bg='black',fg='cyan')
-        button_0.pack()
-
-        label_deci = Label(label_key, bg='black')
-        label_deci.grid(row=3, column=1, padx=10, pady=10)
-        button_deci = Button(label_deci, text='.', font=('Helvetica', '16'),command= lambda: self.click_button('.'),bg='black',fg='cyan')
-        button_deci.pack()
-
-        label_equal = Label(label_key, bg='black')
-        label_equal.grid(row=3, column=2, padx=10, pady=10)
-        button_equal = Button(label_equal, text='=', font=('Helvetica', '16'),command= self.evaluate,bg='black',fg='cyan')
-        button_equal.pack()
-
-        label_C = Label(label_fkey, bg='black')
-        label_C.grid(row=0, column=0,columnspan=2)
-        button_C = Button(label_C, text='C', font=('Helvetica', '16'), height=1, width=10,command=  self.clear,bg='black',fg='cyan')
-        button_C.pack(side=LEFT)
-
-        '''label_del = Label(label_fkey, bg ='black')
-        label_del.grid(row=0,column=1,sticky=E)
-        button_del = Button(label_del, text='del', font=('Helvetica', '16'),bd=3, height=1, width=3,command=  self.delete)
-        button_del.pack()'''
-
-        label_sub = Label(label_fkey, bg='black')
-        label_sub.grid(row=1, column=0, sticky=W, pady=10)
-        button_sub = Button(label_sub, text='-', font=('Helvetica', '16'), height=1, width=3,command= lambda: self.click_button('-'),bg='black',fg='cyan')
-        button_sub.pack(side=LEFT)
-
-        label_mul = Label(label_fkey, bg='black')
-        label_mul.grid(row=1, column=1, sticky=E)
-        button_mul = Button(label_mul, text='x', font=('Helvetica', '16'), height=1, width=3,command= lambda: self.click_button('*'),bg='black',fg='cyan')
-        button_mul.pack()
-
-        label_div = Label(label_fkey, bg='black')
-        label_div.grid(row=2, column=0, sticky=W)
-        button_div = Button(label_div, text='/', font=('Helvetica', '16'), height=1, width=3,command= lambda: self.click_button('/'),bg='black',fg='cyan')
-        button_div.pack()
-
-        label_add = Label(label_fkey, bg='black')
-        label_add.grid(row=2, column=1, sticky=E)
-        button_add = Button(label_add, text='+', font=('Helvetica', '16'), height=1, width=3,command= lambda: self.click_button('+'),bg='black',fg='cyan')
-        button_add.pack()
-
-        label_lbrace = Label(label_fkey, bg='black')
-        label_lbrace.grid(row=3,column=0,sticky=W,pady=10)
-        button_lbrace = Button(label_lbrace,text='(', font=('Helvetica', '16'), height=1, width=3,command= lambda: self.click_button('('),bg='black',fg='cyan')
-        button_lbrace.pack()
-
-        label_rbrace = Label(label_fkey, bg='black')
-        label_rbrace.grid(row=3, column=1, sticky=E, pady=10)
-        button_rbrace = Button(label_rbrace, text=')', font=('Helvetica', '16'), height=1, width=3,
-                               command=lambda: self.click_button(')'),bg='black',fg='cyan')
-        button_rbrace.pack()
-
-c = Calculator(root)
-root.title("Shreejan's calculator")
-root.mainloop()
+ 
+zero = Button(btns_frame, text = "0", fg = "black", width = 21, height = 3, bd = 0, bg = "#fff", cursor = "hand2", command = lambda: btn_click(0)).grid(row = 4, column = 0, columnspan = 2, padx = 1, pady = 1)
+ 
+point = Button(btns_frame, text = ".", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: btn_click(".")).grid(row = 4, column = 2, padx = 1, pady = 1)
+ 
+equals = Button(btns_frame, text = "=", fg = "black", width = 10, height = 3, bd = 0, bg = "#eee", cursor = "hand2", command = lambda: bt_equal()).grid(row = 4, column = 3, padx = 1, pady = 1)
+ 
+win.mainloop()
